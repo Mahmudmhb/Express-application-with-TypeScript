2b0ed00b731c9eab339e284f13c21e065a127ee5
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-05-21 13:33:25 +0600

    create product

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { productsRoute } from "./modules/product/product.route";
 const app: Application = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.use("/api/products", productsRoute);
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello World!");
+// });
 export default app;
---
+++ src/modules/product/product.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import app from "../../app";
+import { ProductServices } from "./product.service";
+
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const product = req.body;
+    const result = await ProductServices.createProductIntoDB(product);
+    res.status(200).json({
+      success: true,
+      message: "Product created successfully!",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const productControllers = {
+  createProduct,
+};
