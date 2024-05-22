750f68d6fbfd4fd77f12145844cf33f9a457cf3b
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-05-22 23:28:04 +0600

    route error handle

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/products", productsRoute);
 app.use("/api/orders", OrdersRoute);
-// app.get("/", (req: Request, res: Response) => {
-//   res.send("Hello World!");
-// });
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
 export default app;
